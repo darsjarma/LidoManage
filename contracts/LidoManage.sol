8a1a033f7a4841ac3bc7dd039d9abd3bd8f31afa
darsjarma <darmajaqfs@gmail.com>
2023-11-08 15:24:37 +0330

    Code cleanup and adding some events.

--- contracts/LidoManage.sol
+++ contracts/LidoManage.sol
@@ -1,102 +1,111 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./interfaces/ILidoWithdrawal.sol";
+import "./interfaces/ILifi.sol";
+import "./interfaces/IStToken.sol";
 
-using SafeMath for uint;
-
-interface IStToken is IERC20{
-    function submit(address _referral) external payable returns (uint256);
-}
-
-
-interface ILidoWithdrawal{
-    function requestWithdrawals(uint256[] calldata _amounts, address _owner) external returns (uint256[] memory requestIds);
-    function claimWithdrawal(uint256 _requestId) external;
-}
-
-
-interface ILifi{
-    struct SwapData {
-        address callTo;
-        address approveTo;
-        address sendingAssetId;
-        address receivingAssetId;
-        uint256 fromAmount;
-        bytes callData;
-        bool requiresDeposit;
-    }
-
-    function swapTokensGeneric(
-        bytes32 _transactionId,
-        string calldata _integrator,
-        string calldata _referrer,
-        address payable _receiver,
-        uint256 _minAmount,
-        SwapData[] calldata _swapData
-    ) external payable;
-}
-
+    using SafeMath for uint;
 
 contract StTokenManage is Ownable {
 
     receive() external payable {}
 
-    address internal stTokenAddress = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
-    address internal stEthWithdrawalAddress = 0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1;
-    address internal lifDiamondAddress = 0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE;
-    address internal usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal stTokenAddress;
+    address internal stEthWithdrawalAddress;
+    address internal lifDiamondAddress;
+    address internal usdcAddress;
 
     AggregatorV3Interface internal stEth_USDPrice;
     AggregatorV3Interface internal USDC_USDPrice;
 
-    uint private stUSDCBalance;
+    event Deposit(uint amount);
+    event RequestWithdrawals(uint[] request_ids);
+    event Claim(uint requestId, uint amount);
 
     constructor(){
+        stTokenAddress = 0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84;
+        stEthWithdrawalAddress = 0x889edC2eDab5f40e902b864aD4d7AdE8E412F9B1;
+        lifDiamondAddress = 0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE;
+        usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
         stEth_USDPrice = AggregatorV3Interface(0xCfE54B5cD566aB89272946F602D76Ea879CAb4a8);
         USDC_USDPrice = AggregatorV3Interface(0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6);
     }
 
-    event RequestWithdrawals(uint[] request_ids);
-
-    function setStTokenAddress(address StTokenAddress) external onlyOwner {
-        stTokenAddress = StTokenAddress;
-    }
-
-    function setStEthWithdrawalAddress(address _stTokenAddress) external onlyOwner {
-        stTokenAddress = _stTokenAddress;
-    }
-
-    function getStEthBalance() public view onlyOwner returns(uint){
+    function getStEthBalance() public view onlyOwner returns (uint){
         return IStToken(stTokenAddress).balanceOf(address(this));
     }
 
-    function getStUSDCBalance() external view onlyOwner returns(uint){
-        return stUSDCBalance;
+    function swapLifi(bool sendsEth, bytes calldata _swapData) public onlyOwner {
+        _swapLifi(sendsEth, _swapData);
     }
 
-    function getTVL() external view onlyOwner returns(int){
-        (,int stEth_USDPriceValue,,,) = stEth_USDPrice.latestRoundData();
-        (,int USDC_USDPriceValue,,,) = USDC_USDPrice.latestRoundData();
-        return int(getStEthBalance())* stEth_USDPriceValue *10*10**6/USDC_USDPriceValue;
+    function setStTokenAddress(address _stTokenAddress) external onlyOwner {
+        stTokenAddress = _stTokenAddress;
+    }
+
+    function setStEthWithdrawalAddress(address _stEthWithdrawalAddress) external onlyOwner {
+        stEthWithdrawalAddress = _stEthWithdrawalAddress;
+    }
+
+    function setLifiDiamondAddress(address _lifiDiamondAddress) external onlyOwner {
+        lifDiamondAddress = _lifiDiamondAddress;
+    }
+
+    function setUSDCAddress(address _usdcAddress) external onlyOwner {
+        usdcAddress = _usdcAddress;
+    }
+
+    function setStEth_USDPrice(AggregatorV3Interface _setStEth_USDPrice) external onlyOwner {
+        stEth_USDPrice = _setStEth_USDPrice;
+    }
+
+    function setUSDC_USDPrice(AggregatorV3Interface _USDC_USDPrice) external onlyOwner {
+        USDC_USDPrice = _USDC_USDPrice;
+    }
+
+
+    function getTVL() external view onlyOwner returns (int){
+        return _getTVL();
     }
 
     function requestWithdrawals(uint[] memory _amounts) external onlyOwner returns (uint256[] memory) {
-        uint sumAmounts;
-        for(uint i = 0 ; i<_amounts.length; i++){
-            sumAmounts +=_amounts[i];
-        }
-        IStToken(stTokenAddress).approve(stEthWithdrawalAddress, sumAmounts);
-        uint[] memory requestIds = ILidoWithdrawal(stEthWithdrawalAddress).requestWithdrawals(_amounts, address(this));
+        uint[] memory requestIds =  _requestWithdrawals(_amounts);
         emit RequestWithdrawals(requestIds);
         return requestIds;
     }
 
-    function claim(bytes calldata _swapData, uint requestId, uint _amount) external onlyOwner{
+    function claim(bytes calldata _swapData, uint requestId, uint _amount) external onlyOwner {
+        _claim(_swapData, requestId, _amount);
+        emit Claim(requestId, _amount);
+    }
+
+
+    function deposit(bytes calldata _swapData, uint _amount) external onlyOwner {
+        _deposit(_swapData, _amount);
+        emit Deposit(_amount);
+    }
+
+    function _getTVL() internal view returns (int){
+        (,int stEth_USDPriceValue,,,) = stEth_USDPrice.latestRoundData();
+        (,int USDC_USDPriceValue,,,) = USDC_USDPrice.latestRoundData();
+        return int(getStEthBalance()) * stEth_USDPriceValue * 10 * 10 ** 6 / USDC_USDPriceValue;
+    }
+
+    function _requestWithdrawals(uint[] memory _amounts) internal returns (uint256[] memory) {
+        uint sumAmounts;
+        for (uint i = 0; i < _amounts.length; i++) {
+            sumAmounts += _amounts[i];
+        }
+        IStToken(stTokenAddress).approve(stEthWithdrawalAddress, sumAmounts);
+        uint[] memory requestIds = ILidoWithdrawal(stEthWithdrawalAddress).requestWithdrawals(_amounts, address(this));
+        return requestIds;
+    }
+
+    function _claim(bytes calldata _swapData, uint requestId, uint _amount) internal {
         uint USDCBalanceBefore = IERC20(usdcAddress).balanceOf(address(this));
         ILidoWithdrawal(stEthWithdrawalAddress).claimWithdrawal(requestId);
         IERC20(usdcAddress).approve(lifDiamondAddress, _amount);
@@ -105,7 +114,7 @@ contract StTokenManage is Ownable {
         IERC20(usdcAddress).transfer(owner(), USDCBalanceAfter - USDCBalanceBefore);
     }
 
-    function swapLifi(bool sendsEth, bytes calldata _swapData) public onlyOwner{
+    function _swapLifi(bool sendsEth, bytes calldata _swapData) internal {
         (
             bytes32 _transactionId,
             string memory _integrator,
@@ -125,7 +134,7 @@ contract StTokenManage is Ownable {
             )
         );
         uint sendEthAmount;
-        if (sendsEth==true)
+        if (sendsEth == true)
             sendEthAmount = _swapsData[0].fromAmount;
         else
             sendEthAmount = 0;
@@ -139,14 +148,12 @@ contract StTokenManage is Ownable {
         );
     }
 
-    function deposit(bytes calldata _swapData, uint _amount) external onlyOwner{
+    function _deposit(bytes calldata _swapData, uint _amount) internal {
         IERC20(usdcAddress).transferFrom(msg.sender, address(this), _amount);
         uint preSwapEthBalance = address(this).balance;
         IERC20(usdcAddress).approve(lifDiamondAddress, _amount);
         swapLifi(false, _swapData);
         uint receivedEth = address(this).balance - preSwapEthBalance;
         IStToken(stTokenAddress).submit{value: receivedEth}(address(this));
-        stUSDCBalance = stUSDCBalance.add(_amount);
     }
 }
-
--- test/LidoManageTest.js
+++ test/LidoManageTest.js
@@ -1,4 +1,4 @@
-const {expect} = require("chai");
+const {expect, assert} = require("chai");
 const {loadFixture} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const {ethers} = require("hardhat");
 const axios = require("axios");
@@ -28,57 +28,56 @@ describe("Lido Management Contract", function(){
     async function deployLidoManageFixture(){
         const [owner, addr1, addr2] = await ethers.getSigners();
         const contractFactory = await ethers.getContractFactory("StTokenManage");
-        const LidoManageContractInstance = await contractFactory.deploy();
-        await LidoManageContractInstance.waitForDeployment();
-        return {LidoManageContractInstance, owner, addr1, addr2};
+        const lidoManageContractInstance = await contractFactory.deploy();
+        await lidoManageContractInstance.waitForDeployment();
+        return {lidoManageContractInstance, owner, addr1, addr2};
     }
     describe("Lido Manage", function(){
         it("Should receive usdc, swap it for eth, submit it, shows stEth value using getTVL method and then request withdrawing a part of it", async()=>{
-            const {LidoManageContractInstance, owner} = await loadFixture(deployLidoManageFixture);
+            const {lidoManageContractInstance, owner} = await loadFixture(deployLidoManageFixture);
             const usdcContract = await ethers.getContractAt(IERC20ApproveAbi, usdcAddress);
-            const LidoManageContractInstanceString = await LidoManageContractInstance.getAddress()
-            const EthToUSDCQuote = await getQuote('ETH', 'ETH', 'ETH', 'USDC', ethers.parseEther('0.1'), LidoManageContractInstanceString, owner.address);
-            const UsdcToEthQuote = await getQuote('ETH', 'ETH', 'USDC', 'ETH', 10*10**6,  LidoManageContractInstanceString, LidoManageContractInstanceString);
+            const lidoManageContractInstanceString = await lidoManageContractInstance.getAddress()
+            const EthToUSDCQuote = await getQuote('ETH', 'ETH', 'ETH', 'USDC', ethers.parseEther('0.1'), lidoManageContractInstanceString, owner.address);
+            const UsdcToEthQuote = await getQuote('ETH', 'ETH', 'USDC', 'ETH', 10*10**6,  lidoManageContractInstanceString, lidoManageContractInstanceString);
             expect(await owner.sendTransaction({
-                    to: LidoManageContractInstanceString,
+                    to: lidoManageContractInstanceString,
                     value: ethers.parseEther("1")
             }
-            )).to.changeEtherBalance([owner, LidoManageContractInstance], [-ethers.parseEther("1"),ethers.parseEther("1")])
-            await LidoManageContractInstance.connect(owner).swapLifi(true, '0x'+EthToUSDCQuote.slice(10));
-            let stUSDCBefore = await LidoManageContractInstance.connect(owner).getStUSDCBalance();
-            await usdcContract.connect(owner).approve(LidoManageContractInstance.getAddress(), 10000000);
-            let stEthBefore = await LidoManageContractInstance.connect(owner).getStEthBalance();
-            await LidoManageContractInstance.connect(owner).deposit(
+            )).to.changeEtherBalance([owner, lidoManageContractInstance], [-ethers.parseEther("1"),ethers.parseEther("1")])
+            await lidoManageContractInstance.connect(owner).swapLifi(true, '0x'+EthToUSDCQuote.slice(10));
+            // await owner.sendTransaction()
+            // usdcContract.connect(owner).callStatic
+            await usdcContract.connect(owner).approve(lidoManageContractInstance.getAddress(), 10000000);
+            let stEthBefore = await lidoManageContractInstance.connect(owner).getStEthBalance();
+            await lidoManageContractInstance.connect(owner).deposit(
                 '0x'+UsdcToEthQuote.slice(10),
                 10000000);
-            let stUSDCAfter = await LidoManageContractInstance.connect(owner).getStUSDCBalance();
-            let stEthAfter = await LidoManageContractInstance.connect(owner).getStEthBalance();
+            let stEthAfter = await lidoManageContractInstance.connect(owner).getStEthBalance();
             console.log(`Increased staked eth ${ethers.formatEther(stEthAfter-stEthBefore)} total staked: ${ethers.formatEther(stEthAfter)}`);
             expect(stEthAfter-stEthBefore).to.not.equal(0);
-            let stValue = await LidoManageContractInstance.connect(owner).getTVL();
+            let stValue = await lidoManageContractInstance.connect(owner).getTVL();
+            assert(Number(stValue)/(10*10**(6+18)) >= 7);
             console.log('stEth value in USDC is:',  Number(stValue)/(10*10**(6+18)));
-            expect(stUSDCAfter-stUSDCBefore).to.equals(10000000);
-            await LidoManageContractInstance.connect(owner).requestWithdrawals([100]);
-            let filter = LidoManageContractInstance.filters.RequestWithdrawals
-            let events = await LidoManageContractInstance.queryFilter(filter, -1)
+            await lidoManageContractInstance.connect(owner).requestWithdrawals([100]);
+            let filter = lidoManageContractInstance.filters.RequestWithdrawals
+            let events = await lidoManageContractInstance.queryFilter(filter, -1)
             console.log(events[0].args.requestIDs);
         }).timeout(10000000)
     })
 
     it("Should revert when a non-owner calls any of the function methods", async()=>{
-       const {LidoManageContractInstance, owner, addr1} = await loadFixture(deployLidoManageFixture);
-            const LidoManageContractInstanceString = await LidoManageContractInstance.getAddress()
+       const {lidoManageContractInstance, owner, addr1} = await loadFixture(deployLidoManageFixture);
+            const LidoManageContractInstanceString = await lidoManageContractInstance.getAddress()
             const EthToUSDCQuote = await getQuote('ETH', 'ETH', 'ETH', 'USDC', ethers.parseEther('0.1'), LidoManageContractInstanceString, owner.address);
             const UsdcToEthQuote = await getQuote('ETH', 'ETH', 'USDC', 'ETH', 10*10**6,  LidoManageContractInstanceString, LidoManageContractInstanceString);
-            expect(LidoManageContractInstance.connect(addr1).getStUSDCBalance()).to.reverted;
-            expect(LidoManageContractInstance.connect(addr1).setStTokenAddress(owner.address)).to.reverted;
-            expect(LidoManageContractInstance.connect(addr1).setStEthWithdrawalAddress(owner.address)).to.reverted;
-            expect(LidoManageContractInstance.connect(addr1).getStEthBalance(owner.address)).to.reverted;
-            expect(LidoManageContractInstance.connect(addr1).getTVL()).to.reverted;
-            expect(LidoManageContractInstance.connect(addr1).requestWithdrawals([100])).to.reverted;
-            expect(LidoManageContractInstance.connect(addr1).claim(1)).to.reverted;
-            expect(LidoManageContractInstance.connect(addr1).swapLifi(true, EthToUSDCQuote)).to.reverted;
-            expect(LidoManageContractInstance.connect(addr1).deposit('0x'+UsdcToEthQuote.slice(10), 10000000)).to.reverted;
+            expect(lidoManageContractInstance.connect(addr1).setStTokenAddress(owner.address)).to.reverted;
+            expect(lidoManageContractInstance.connect(addr1).setStEthWithdrawalAddress(owner.address)).to.reverted;
+            expect(lidoManageContractInstance.connect(addr1).getStEthBalance(owner.address)).to.reverted;
+            expect(lidoManageContractInstance.connect(addr1).getTVL()).to.reverted;
+            expect(lidoManageContractInstance.connect(addr1).requestWithdrawals([100])).to.reverted;
+            expect(lidoManageContractInstance.connect(addr1).claim(1)).to.reverted;
+            expect(lidoManageContractInstance.connect(addr1).swapLifi(true, EthToUSDCQuote)).to.reverted;
+            expect(lidoManageContractInstance.connect(addr1).deposit('0x'+UsdcToEthQuote.slice(10), 10000000)).to.reverted;
         }
     ).timeout(100000)
 
